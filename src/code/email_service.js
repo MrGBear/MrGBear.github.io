03c291cc13053a8602535356dee5c635f95ae5fb
Sven Boerrigter <sven.boerrigter@telekom.de>
2025-03-13 11:50:18 +0100

    fix: mail_service;

--- src/code/email_service.js
+++ src/code/email_service.js
@@ -1,16 +1,37 @@
+/**
+ * Create a personal email text
+ * @param {*} player_name The Name of the player
+ * @param {*} ega_hdci The new ega value
+ * @param {*} whs_hdci The new whs value
+ * @returns The email text
+ */
 function create_mail_text(player_name, ega_hdci, whs_hdci) {
-    const email = `
-    Hallo ${player_name},
+    const email = `Hallo ${player_name},
 
-    Uns liegt ein Update zu ihrem Ergebnissen vor, weswegen wir das Handicap neu berechnet haben.
+Uns liegt ein Update zu deinem Ergebnissen vor, weswegen wir das Handicap neu berechnet haben.
 
-    Ihr neues Handicap nach dem EGA-System ist: ${ega_hdci},
-    Ihr neues Handicap nach dem WHS-System ist: ${whs_hdci}
+Dein neues Handicap nach dem EGA-System ist: ${ega_hdci},
+Dein neues Handicap nach dem WHS-System ist: ${whs_hdci}
 
-    Wir wünschen dir einen angenehmen Tag und freuen uns auf deinen nächsten Besuch!
-    
-    Mit freundlichen Grüßen
-    Ihr Golfteam
-    `
+Wir wünschen dir einen angenehmen Tag und freuen uns auf deinen nächsten Besuch!
+
+Mit freundlichen Grüßen
+Ihr Golfteam
+`
     return email;
+}
+
+/**
+ * Send an email updating them of a change in their handicap
+ * @param {*} player_name The Name of the player
+ * @param {*} ega_hdci The new ega value
+ * @param {*} whs_hdci The new whs value
+ * @param {*} recipient_email Email of the player
+ */
+export function send_email(player_name, ega_hdci, whs_hdci, recipient_email) {
+    const subject = "Handicap Update";
+    const body = create_mail_text(player_name, ega_hdci, whs_hdci);
+    const mailtoLink = `mailto:${recipient_email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+
+    window.location.href = mailtoLink;
 }
---
+++ src/test/unittests/email_service/email_service.test.js
@@ -0,0 +1,2 @@
+import { send_email } from "../../../code/email_service.js";
+send_email("TestPlayer", 0, 40, "test@mail")
